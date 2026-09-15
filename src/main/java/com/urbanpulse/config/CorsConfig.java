925b845d28433dac1a14113946128a089d228e5e
izzy-erik1 <israel.erkalo@stud.ubbcluj.ro>
2026-09-15 22:40:58 +0300

    feat: changes patch mapping is added.

--- src/main/java/com/urbanpulse/config/CorsConfig.java
+++ src/main/java/com/urbanpulse/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
     public FilterRegistrationBean<CorsFilter> corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("http://localhost:5173"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
 
